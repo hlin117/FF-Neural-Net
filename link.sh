24217128ee0d1679c97067ffa172ac9f53121384
hlin117 <hlin117@gmail.com>
2014-11-22 13:02:52 -0600

    modified link.sh to allow removing old linked files

--- link.sh
+++ link.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
+lin_sep=("classneuralnet.py" "neuralnet.py")
+
+# For the case that we just want to remove all of the generated
+# links and the .gitignore
+if [ "$1" = "reset" ]; then
+
+	if [ -f demo/.gitignore ]; then
+		rm demo/.gitignore
+	fi
+
+	for i in "${lin_sep[@]}"; do
+		if [ -f demo/lin_separable/$i ]; then
+			rm demo/lin_separable/$i
+		fi
+	done
+	echo "Removed ./demo/.gitignore and other links"
+	exit 0
+fi
+
+# Normal case: when we want to only create the files
 if [ ! -f demo/.gitignore ]; then
 	touch ./demo/.gitignore
 	echo "Created ./demo/.gitignore"
+else
+	echo "./demo/.gitignore detected. run './link reset' to remove files"
+	exit 0
 fi
 
 # Creates hard links for the neural net model from /model to /demo
 # TODO: Just write a recursive function for this instead of manually
 # linking each one...
-lin_sep=("classneuralnet.py" "neuralnet.py")
 
 for i in "${lin_sep[@]}"; do
 	if [ -f demo/lin_separable/$i ]; then
