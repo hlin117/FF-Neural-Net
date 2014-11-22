385a14489896dd3263889d9286313d597328c223
hlin117 <hlin117@gmail.com>
2014-11-21 20:50:58 -0600

    Wrote basic feedforward algorithm

--- model/neuralnet.py
+++ model/neuralnet.py
@@ -22,12 +22,12 @@ class NeuralNet(object):
         # TODO: Address this.
         if num_output != 1:
             raise NotImplementedError("Neural network containing more than one \
-            output label has not been implemented yet.")
+                    output label has not been implemented yet.")
 
         # TODO: Address this.
         if len(hidden_layer) > 1
             raise NotImplementedError("Neural network containing more than one \
-            hidden layer has not been implemented yet.")
+                    hidden layer has not been implemented yet.")
 
         self.num_features = num_features
         self.num_output = num_output
@@ -38,7 +38,7 @@ class NeuralNet(object):
             self.default_act = expit
         else:
             raise NotImplementedError("Neural network that uses a default function \
-            other than the sigmoid function is not yet implemented.")
+                    other than the sigmoid function is not yet implemented.")
 
         # List of the weight matrices here. If w_ij(k) is a weight in the matrix,
         # it is the weight assigned to the edge from neuron i to neuron j in the
@@ -55,5 +55,54 @@ class NeuralNet(object):
         self.weights1[-1] = 1
         self.weights2[-1] = 1
 
+    def verity_data(self, data):
+        """Verifies that the data is in the form of a nested iterable, and 
+        that each is of the length self.num_features. Also verifies that
+        each inner nested object is a float type object.
+
+        Might want to introduce a command that disables this feature, but
+        as far as I know, only the training process is known to take much time.
+        """
+        for sample in data:
+            if len(sample) != self.num_features:
+                raise ValueError("Input data is not of the same length \
+                        as the number of input neurons: {0}".format(innerlist))
+
+            for feature in sample:
+                if isinstance(feature, (int, float)):
+                    raise ValueError("Detected feature that is not compatible \
+                            with the Neural Network: {0}".format(feature))
 
 
+
+    def train(self, data, targets)
+        """Trains the neural network on a set of data. Data should be
+        in the form of two nested ordered iterables. Uses the backpropagation
+        algorithm to adjust the weights of the edges.
+
+        Each inner iterable should be of length num_features. If not, then
+        a ValueError is raised."""
+        self.verify_data(data)
+        for sample in data:
+            outputs = self.feed_forward(sample) 
+            errors = self.backpropagate(outputs, targets)
+            update_weights(errors)
+
+    def feed_forward(self, sample):
+        """Obtains the output from a feedforward computation.
+        
+        NOTE: Still under development. The constructor of the neural
+        network should prevent any NN with more than one hidden layer
+        from being constructed."""
+        input_aug = np.array(sample, dtype=float).append(1)
+        excite1 = input_aug * self.weights1
+        output1 = self.default_act(excite1)
+
+        output1_aug = output1.append(1)
+        excite2 = output1_aug * self.weights2
+        output2 = self.default_act(excite2)
+
+        return (output1, output2)
+
+    def backpropagate(outputs, targets):
+        pass
