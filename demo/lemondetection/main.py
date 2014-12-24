0fe14685c0c49b6bc17ef534b7cbc15a2d3a6c8a
hlin117 <hlin117@gmail.com>
2014-12-24 13:00:08 -0600

    created data for training and testing

--- demo/lemondetection/main.py
+++ demo/lemondetection/main.py
@@ -1,43 +1,43 @@
 #!/usr/bin/python
+import csv
 import sys
+from neuralnet import NeuralNet
 from time import time
 import numpy as np
-from classneuralnet import ClassifierNeuralNet
 from random import shuffle
 
 def main():
     """Testing file to show neural network can learn linearly separable
     data."""
-    data = np.genfromtxt("binary.txt", delimiter=',').tolist()
+    print "Reading from the text file..."
+    #data = np.genfromtxt("training.csv", delimiter=',').tolist()
+    print "Shuffling..."
+    shuffle(data)
 
+    # NOTE: We have to wrap every target value into a tuple, for the
+    # purpose of being able to classify n-tuples later
+    targets = np.array(tuple((sample[-1],) for sample in data))
+    features = tuple(sample[:-1] for sample in data)
+    print "Starting to train..."
+    start = time()
 
-    for i in xrange(10):
-        shuffle(data)
+    num_features = len(data[0]) - 1  # Subtract one because of target values
+    nn = NeuralNet(num_features, max_epochs=2, learn_rate=.85, scale=0.1, 
+                   hidden_layer=[7], verbose=True)
+    nn.train(features, targets)
+    print "Done with training. Took {0} seconds to train." \
+            .format(round(time() - start, 2))
 
-        # NOTE: We have to wrap every target value into a tuple, for the
-        # purpose of being able to classify n-tuples later
-        targets = np.array(tuple((sample[-1],) for sample in data))
-        features = tuple(sample[:-1] for sample in data)
-        print "Starting to train..."
-        start = time()
+    print "Beginning with scoring..."
+    start = time()
+    testing = np.genfromtxt("testing.csv", delimiter=",")
+    predictions = nn.score_data(testing)
+    print "Done with scoring. Took {0} seconds to score the dataset" \
+            .format(round(time() - start, 2))
 
-        num_features = len(data[0]) - 1  # Subtract one because of target values
-        nn = ClassifierNeuralNet(num_features, max_epochs=2,
-                learn_rate=.85, scale=0.1, verbose=True)
-        nn.train(features, targets)
-        print "Done with training. Took {0} seconds to train." \
-                .format(round(time() - start, 2))
-
-        print "Beginning with scoring..."
-        start = time()
-        scored_data = np.genfromtxt("output.txt_features", delimiter=",")
-        correct = np.genfromtxt("output.txt_targets", delimiter=",")
-        prediction = nn.score_data(scored_data)
-        print "Done with scoring. Took {0} seconds to score the dataset" \
-                .format(round(time() - start, 2))
-
-        print "Total number incorrect: {0}".format(sum(1 for i in \
-                xrange(len(correct)) if correct[i] != prediction[i]))
+    with open("results.txt", "w") as f:
+        for pred in predictions:
+            f.write(str(pred) + "\n")
 
 
 if __name__ == "__main__":
