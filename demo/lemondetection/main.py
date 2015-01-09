f2aac5f7150054e29f40b382642f0d59ef4dfe6b
hlin117 <hlin117@gmail.com>
2015-01-08 19:00:33 -0600

    going to check numbers against backpropogation formula. #17

--- demo/lemondetection/main.py
+++ demo/lemondetection/main.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python
-import csv
-import sys
 from neuralnet import NeuralNet
 from time import time
 import numpy as np
@@ -9,7 +7,7 @@ from random import shuffle
 def main():
 
     print "Loading in the data..."
-    text = open("training.csv").read().split("\n")
+    text = open("small.csv").read().split("\n")
     data = list(map(int, sample.strip().split(",")) for sample in text
                 if sample.strip() != "")
     print "Shuffling..."
@@ -17,14 +15,14 @@ def main():
 
     # NOTE: We have to wrap every target value into a tuple, for the
     # purpose of being able to classify n-tuples later
-    targets = np.array(tuple((1 if sample[-1] == 1 else 0,) for sample in data))
+    targets = tuple((1 if sample[-1] == 1 else 0,) for sample in data)
     features = tuple(sample[:-1] for sample in data)
     print "Starting to train..."
     start = time()
 
     num_features = len(data[0]) - 1  # Subtract one because of target values
 
-    nn = NeuralNet(num_features, max_epochs=2, learn_rate=.7, scale=0.1, 
+    nn = NeuralNet(num_features, max_epochs=20, learn_rate=.7, scale=0.1, 
                    hidden_layer=[4], verbose=True, activation=("tanh", 1))
     nn.train(features, targets)
 
