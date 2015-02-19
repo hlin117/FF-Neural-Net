d12eed5bf512e35c6dbad9986b1070bf297c5497
Henry Lin <hlin117@gmail.com>
2015-02-19 03:20:49 -0600

    still debugging...

--- model/neuralnet.py
+++ model/neuralnet.py
@@ -1,4 +1,5 @@
 from __future__ import print_function, division
+from sys import exit
 import json
 #from scipy.stats import logistic
 #from scipy.special import expit
@@ -126,8 +127,8 @@ class NeuralNet(object):
         hidden layer.
         """
         rescale = lambda matrix: self.scale * matrix - self.scale / 2
-        self.weights1 = np.zeros((self.num_features + 1, self.hidden_layer[0])) # np.mat(rescale(np.random.rand(self.num_features + 1, self.hidden_layer[0])))
-        self.weights2 = np.zeros((self.hidden_layer[0] + 1))# np.mat(rescale(np.random.rand(self.hidden_layer[0] + 1, self.num_output)))
+        self.weights1 = np.mat(np.zeros((self.num_features + 1, self.hidden_layer[0]))) # np.mat(rescale(np.random.rand(self.num_features + 1, self.hidden_layer[0])))
+        self.weights2 = np.mat(np.zeros((self.hidden_layer[0] + 1)))# np.mat(rescale(np.random.rand(self.hidden_layer[0] + 1, self.num_output)))
         if self.default_bias != "random":
             self.weights1[-1, :] = self.default_bias
             self.weights2[-1, :] = self.default_bias
@@ -185,6 +186,7 @@ class NeuralNet(object):
                     change2 += (-self.learn_rate * deltas[1] * outputs[1]).T
                 print(self.weights1)
                 print(self.weights2)
+                exit(0)
             if not self.online:
                 self.weights1 += change1
                 self.weights2 += change2
